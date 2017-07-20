8f52d810287251d97ae9bce1196d9e4390485be1
David Medina Ospina <davidmedinaospina@Davids-MacBook-Pro.local>
2017-07-19 20:34:00 -0500

    Solving bugs

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ app.get('/', (req, res) => {
     res.send('Use API');
 });
 
+app.use(cors());
+app.use(bodyParser.json());
 app.use('/users', users);
 app.use('/songs', songs);
-app.use(bodyParser.json());
-app.use(cors());
 
 app.listen(3000);
 console.log('Running on port 3000');
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+
 router.get('/getAllUsers', (req, res) => {
     User.getUsers((err, users) => {
         if(err) {
