bf577a1fd0172adf88000e60c630205bb4491668
David Medina Ospina <davidmedinaospina@Davids-MacBook-Pro.local>
2017-08-02 17:41:23 -0500

    :v

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/canopee',
+    //database: 'mongodb://localhost:27017/canopee',
+    database: 'mongodb://dameos:Ajiaco312@ds023510.mlab.com:23510/canopee'
 }
